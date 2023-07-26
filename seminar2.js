e3e1c19931d1230d0c0f8dbc0c4daeb3b63e319b
ZhenyaGro <bon4ios@gmail.com>
2023-07-26 11:49:21 +0300

    Correct functions name numbers

--- seminar2.js
+++ seminar2.js
@@ -1,10 +1,10 @@
-console.log(task1());
-task2();
-console.log(task3());
-task3plus();
+console.log(task2_1());
+task2_2();
+console.log(task2_3());
+task2_3plus();
 
 
-function task1() {
+function task2_1() {
   /*
   Задание 1
   Создать переменные num1, num2 которые пользователь вводит с клавиатуры
@@ -16,7 +16,7 @@ function task1() {
   return num1 <= 1 && num2 >= 3;
 }
 
-function task2() {
+function task2_2() {
   /*
   Задание 2
   Перепишите код к тернарному оператору
@@ -33,7 +33,7 @@ function task2() {
   test ? console.log('+++') : console.log('---');
 }
 
-function task3() {
+function task2_3() {
   /*
   Задание 3
   В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
@@ -47,7 +47,7 @@ function task3() {
   return 'Третья декада';
 }
 
-function task3plus() {
+function task2_3plus() {
   const day = Math.round(Math.random() * (31 - 1) + 1);
   console.log(`День: ${day}`);
 
