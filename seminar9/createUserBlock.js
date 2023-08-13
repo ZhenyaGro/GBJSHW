ea73227c915d5a33b052aba7aad28caf3f47114e
ZhenyaGro <bon4ios@gmail.com>
2023-08-13 15:14:59 +0300

    Solution for task 1 seminar 9

--- seminar9/createUserBlock.js
+++ seminar9/createUserBlock.js
@@ -1,24 +1,20 @@
-function createUserBlock() {
-  let divCount = 0;
+export function createUserBlock(elWhere, data) {
+  const userId = JSON.parse(data).id;
 
-  return function (elWhere, data) {
-    const newDiv = document.createElement("div");
-    newDiv.appendChild(document.createTextNode(data));
-    newDiv.classList.add('main__block');
-    newDiv.setAttribute('id', `block-${JSON.parse(data).id}`);
+  const newDiv = document.createElement("div");
+  newDiv.appendChild(document.createTextNode(data));
+  newDiv.classList.add('main__block');
+  newDiv.setAttribute('id', `block-${userId}`);
 
-    document.querySelector(elWhere).insertAdjacentElement('beforeend', newDiv);
+  document.querySelector(elWhere).insertAdjacentElement('beforeend', newDiv);
 
-    const buttonRemove = document.createElement("button");
-    buttonRemove.appendChild(document.createTextNode('X'));
-    buttonRemove.classList.add('main__button-remove');
-    buttonRemove.setAttribute('id', `button-${JSON.parse(data).id}`);
-    document.getElementById(`block-${JSON.parse(data).id}`).insertAdjacentElement('beforeend', buttonRemove);
-    buttonRemove.addEventListener('click', e => {
-      localStorage.removeItem(`User № ${e.target.id.replace('button-', '')}`);
-      document.getElementById(`block-${e.target.id.replace('button-', '')}`).remove();
-    });
-  }
+  const buttonRemove = document.createElement("button");
+  buttonRemove.appendChild(document.createTextNode('X'));
+  buttonRemove.classList.add('main__button-remove');
+  buttonRemove.setAttribute('id', `button-${userId}`);
+  document.getElementById(`block-${userId}`).insertAdjacentElement('beforeend', buttonRemove);
+  buttonRemove.addEventListener('click', e => {
+    localStorage.removeItem(`User № ${e.target.id.replace('button-', '')}`);
+    document.getElementById(`block-${e.target.id.replace('button-', '')}`).remove();
+  });
 }
-
-export const createNewBlock = createUserBlock();
--- seminar9/seminar9_1.js
+++ seminar9/seminar9_1.js
@@ -3,23 +3,25 @@
 Необходимо получить список всех пользователей с помощью бесплатного API (https://jsonplaceholder.typicode.com/users) и отобразить их на странице. Пользователь должен иметь возможность удалить любого пользователя из списка. Данные при получении необходимо сохранить в локальное хранилище браузера localStorage. При удалении пользователь должен удаляться не только со страницы, но и из локального хранилища localStorage
 */
 
-// Show users
-// Remove user + remove from localStorage
-
 import { getData } from './getData.js';
-import { createDiv } from './createDiv.js';
+import { createUserBlock } from './createUserBlock.js';
 
 const url = 'https://jsonplaceholder.typicode.com/users';
 const htmlContent = document.querySelector('.main__content');
 
-try {
-  const users = await getData(url);
-  users.forEach((user, index) => {
-    const strUser = JSON.stringify(user);
-    localStorage.setItem(`User № ${index + 1}`, strUser)
-    createDiv('.main__content', strUser);
-  });
-  console.log(users);
-} catch (error) {
-  console.error(`Ошибка: ${error}`);
-}
+document.querySelector('.main__button').addEventListener('click', async () => {
+  try {
+    document.querySelector('.main__content').innerHTML = '';
+
+    const users = await getData(url);
+    users.forEach((user, index) => {
+      const strUser = JSON.stringify(user);
+      localStorage.setItem(`User № ${index + 1}`, strUser)
+      createUserBlock('.main__content', strUser);
+    });
+    console.log(users);
+  } catch (error) {
+    console.error(`Ошибка: ${error}`);
+  }
+});
+
