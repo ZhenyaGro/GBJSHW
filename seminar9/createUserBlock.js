83b66a41bfc42e74e18d94335e220e71ba8c09f6
ZhenyaGro <bon4ios@gmail.com>
2023-08-13 14:50:26 +0300

    Rename, add buttons id

--- seminar9/createUserBlock.js
+++ seminar9/createUserBlock.js
@@ -1,4 +1,4 @@
-function createDiv() {
+function createUserBlock() {
   let divCount = 0;
 
   return function (elWhere, data) {
@@ -12,8 +12,10 @@ function createDiv() {
     const buttonRemove = document.createElement("button");
     buttonRemove.appendChild(document.createTextNode('X'));
     buttonRemove.classList.add('main__button-remove');
+    buttonRemove.setAttribute('id', `button-${divCount}`);
     document.getElementById(`block-${divCount}`).insertAdjacentElement('beforeend', buttonRemove);
+    buttonRemove.addEventListener('click', e => console.log(`click! ${e.target.id}`))
   }
 }
 
-export const createNewDiv = createDiv();
+export const createNewBlock = createUserBlock();
