45810c184ee24f99e253bcb065e132a7cf45d69c
ZhenyaGro <bon4ios@gmail.com>
2023-08-13 14:41:56 +0300

    Function closure

--- seminar9/createDiv.js
+++ seminar9/createDiv.js
@@ -1,7 +1,19 @@
-export function createDiv(elWhere, data) {
-  const newDiv = document.createElement("div");
-  newDiv.appendChild(document.createTextNode(data));
-  newDiv.classList.add('main__block');
+function createDiv() {
+  let divCount = 0;
 
-  document.querySelector(elWhere).insertAdjacentElement('beforeend', newDiv);
+  return function (elWhere, data) {
+    const newDiv = document.createElement("div");
+    newDiv.appendChild(document.createTextNode(data));
+    newDiv.classList.add('main__block');
+    newDiv.setAttribute('id', `block-${++divCount}`);
+
+    document.querySelector(elWhere).insertAdjacentElement('beforeend', newDiv);
+
+    const buttonRemove = document.createElement("button");
+    buttonRemove.appendChild(document.createTextNode('X'));
+    buttonRemove.classList.add('main__button-remove');
+    document.getElementById(`block-${divCount}`).insertAdjacentElement('beforeend', buttonRemove);
+  }
 }
+
+export const createNewDiv = createDiv();
