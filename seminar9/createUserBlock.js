7af907f57479fc2a033c7a7b1ddd2e6d2dfdc120
ZhenyaGro <bon4ios@gmail.com>
2023-08-13 15:10:07 +0300

    Use id from data

--- seminar9/createUserBlock.js
+++ seminar9/createUserBlock.js
@@ -5,15 +5,15 @@ function createUserBlock() {
     const newDiv = document.createElement("div");
     newDiv.appendChild(document.createTextNode(data));
     newDiv.classList.add('main__block');
-    newDiv.setAttribute('id', `block-${++divCount}`);
+    newDiv.setAttribute('id', `block-${JSON.parse(data).id}`);
 
     document.querySelector(elWhere).insertAdjacentElement('beforeend', newDiv);
 
     const buttonRemove = document.createElement("button");
     buttonRemove.appendChild(document.createTextNode('X'));
     buttonRemove.classList.add('main__button-remove');
-    buttonRemove.setAttribute('id', `button-${divCount}`);
-    document.getElementById(`block-${divCount}`).insertAdjacentElement('beforeend', buttonRemove);
+    buttonRemove.setAttribute('id', `button-${JSON.parse(data).id}`);
+    document.getElementById(`block-${JSON.parse(data).id}`).insertAdjacentElement('beforeend', buttonRemove);
     buttonRemove.addEventListener('click', e => {
       localStorage.removeItem(`User № ${e.target.id.replace('button-', '')}`);
       document.getElementById(`block-${e.target.id.replace('button-', '')}`).remove();
